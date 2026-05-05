13c1fa76c4e94154cfc1d3acb4d4e67606c4043d
481n4y4 <kheiraabinaya14@gmail.com>
2026-05-05 13:54:19 +0700

    🔥 api(api): comment api is not needed

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
-Route::get('/book', [BookController::class, 'index']);
-Route::post('/book', [BookController::class, 'store']);
-Route::get('/book/{id}', [BookController::class, 'show']);
-Route::put('/book/{id}', [BookController::class, 'update']);
-Route::delete('/book/{id}', [BookController::class, 'destroy']);
+// Route::get('/book', [BookController::class, 'index']);
+// Route::post('/book', [BookController::class, 'store']);
+// Route::get('/book/{id}', [BookController::class, 'show']);
+// Route::put('/book/{id}', [BookController::class, 'update']);
+// Route::delete('/book/{id}', [BookController::class, 'destroy']);
 
 Route::prefix('/book')->controller(BookController::class)->group(function () {
     Route::get('/', 'index');
