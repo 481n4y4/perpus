442aa40ba3243e4574ec2b3615b80c1cd85a1432
481n4y4 <kheiraabinaya14@gmail.com>
2026-05-05 09:54:35 +0700

    📌 chore(api): creating crud controller

---
+++ backend/app/Http/Controllers/BookController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $book = Book::all();
+
+        return response()->json([
+            'message' => 'Berhasil mengambil data buku',
+            'data' => $book
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|max:50|string',
+            'author' => 'required|max:50|string',
+            'publisher' => 'required|max:50|string',
+            'publish-date' => 'required|max_digits:10|integer',
+            'price' => 'required|max_digits:10|integer',
+            'stock' => 'required|max_digits:10|integer',
+            'cover_book' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
+        ]);
+
+        if ($request->hasFile('cover_book')) {
+            $validated['cover_book'] = $request->file('cover_book')->store('books', 'public');
+        };
+
+        $book = Book::create($validated);
+
+        return response()->json([
+            'message' => 'berhasil menambahkan data buku: ' . $book->name,
+            'data' => $book
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        $book = Book::findOrFail($id);
+
+        return response()->json([
+            'message' => 'berhasil mengambil data buku: ' . $book->name,
+            'data' => $book
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        $book = Book::findOrFail($id);
+
+        $validated = $request->validate([
+            'name' => 'required|max:50|string',
+            'author' => 'required|max:50|string',
+            'publisher' => 'required|max:50|string',
+            'publish-date' => 'required|max_digits:10|integer',
+            'price' => 'required|max_digits:10|integer',
+            'stock' => 'required|max_digits:10|integer',
+            'cover_book' => 'nullable|image|mimes:jpg,jpeg,png|max:2048'
+        ]);
+
+        if ($request->hasFile('cover_book')) {
+            $validated['cover_book'] = $request->file('cover_book')->store('books', 'public');
+        };
+
+        $book->update($validated);
+
+        return response()->json([
+            'message' => 'berhasil mengupdate data buku: ' . $book->name,
+            'data' => $book
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        $book = Book::findOrFail($id);
+        $book->delete();
+
+        return reponse()->json([
+            'message' => 'berhasil menghapus data buku',
+            'data' => $book
+        ]);
+    }
+}
